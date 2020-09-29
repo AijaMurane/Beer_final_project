dbf990ff7bc74851ffa9708afbdf5691daac6154
Aija Murāne <aija.murane@gmail.com>
2020-09-29 19:54:47 +0300

    Split the code in several objects

--- src/main/scala/Beers.scala
+++ src/main/scala/Beers.scala
@@ -1,8 +1,3 @@
-import java.io.FileWriter
-import java.sql.{Driver, DriverManager}
-import java.util.Arrays
-import scala.collection.mutable.ListBuffer
-
 /** Extracts words from articles about one topic - beer. Filter the most common words.*/
 
 object Beers extends App {
@@ -22,182 +17,21 @@ object Beers extends App {
     myString
   }
 
-  /** The string is split into separate words and saved into array.
-   Then the array is transferred to a sequence with only unique values along with number of occurrences of that word.*/
-  def WordCount(myString: String): Array[(String,Int)] = {
-
-    //getting rid of characters like dot and comma that we do not need and getting rid of words like "and", "or" etc. too
-    val onlyWordsString = myString.replaceAll("[^a-zA-Z\\s]","").toLowerCase
-
-    val wrongWords = Seq("with", "that", "from", "have", "which", "were", "this", "they", "their", "these", "there", "what", "other")
-
-    val myArray = onlyWordsString.split("\\s+")
-      .filter(x => x.length > 3 || x == "ale" || x == "ipa" || x == "dry")
-      .filterNot(x => wrongWords.exists(y => x.contains(y)))
-
-    //splitting text with white space word by word and getting rid of unnecessary words
-   /** val myArray = onlyWordsString.split("\\s+").filterNot(i =>
-        i.contains("of") ||
-        i.contains("the") ||
-        i.contains("and") ||
-        i.contains("a") ||
-        i.contains("of") ||
-        i.contains("to") ||
-        i.contains("in") ||
-        i.contains("is") ||
-        i.contains("with") ||
-        i.contains("that") ||
-        i.contains("as") ||
-        i.contains("was") ||
-        i.contains("are") ||
-        i.contains("its") ||
-        i.contains("but") ||
-        i.contains("it") ||
-        i.contains("for") ||
-        i.contains("be") ||
-        i.contains("can") ||
-        i.contains("or") ||
-        i.contains("by")) //FIXME add more unnecessary words and maybe create a function for this. I tried, but did not manage to mke it work*/
-
-     //creating a tuple of a word and how many times it occurs in the text
-    val arrayTuples = myArray.groupMapReduce(identity)(_ => 1)(_ + _)
-
-    //filtering unique values and sorting them from most to least frequent words
-    val uniqueTuples = arrayTuples.toSet.toArray.sortBy(_._2).reverse
-
-    /**the printing is for checking the result
-    for (w <- uniqueTuples) {
-      println(w)
-    } */
-
-    uniqueTuples
-    }
-
-  def createNewDatabase() = {
-    val environmentVars = System.getenv()
-
-    //val properties = System.getProperties()
-    val sqlite_home = environmentVars.get("SQLITE_HOME").replace("\\", "/")
-
-    val dbname = "beersDB.db"
-    println(s"Creating DB $dbname")
-    val url = s"jdbc:sqlite:$sqlite_home/db/$dbname"
-
-    val conn = DriverManager.getConnection(url)
-
-    //lets make a table!
-    val sql =
-      """
-        |CREATE TABLE IF NOT EXISTS beersTable (
-        |	word_id INTEGER PRIMARY KEY,
-        |	word TEXT NOT NULL,
-        |	word_count INTEGER NOT NULL);
-        |""".stripMargin
-
-    val statement = conn.createStatement()
-    val resultSet = statement.execute(sql)
-  }
-
-  val beerDB = createNewDatabase()
-
-  def writeToDatabase(beerArray: Array[(String,Int)], fName: String) {
-    val insertSql =
-      """
-        |INSERT INTO beersTable (word_id, word, word_count)
-        |VALUES(?,?,?)
-        |""".stripMargin
-
-    val environmentVars = System.getenv()
-    val sqlite_home = environmentVars.get("SQLITE_HOME").replace("\\", "/")
-    val dbname = fName
-    val url = s"jdbc:sqlite:$sqlite_home/db/$dbname"
-
-    val conn = DriverManager.getConnection(url)
-
-    val pstmt = conn.prepareStatement(insertSql)
-
-    for (r <- beerArray) {
-      val statement = conn.createStatement()
-
-      val checkID =
-        """
-          |SELECT word_id FROM beersTable
-          |ORDER BY word_id
-          |DESC
-          |LIMIT 1
-          |""".stripMargin
-
-      val resultSet = statement.executeQuery(checkID)
-
-      val lastID = {
-        try {
-          resultSet.getInt(1)
-        } catch {
-          case e: Exception => 0
-        }
-      }
-
-      val word_id = lastID + 1
-      val word = r._1
-      val word_count = r._2
-
-      pstmt.setInt(1, word_id)
-      pstmt.setString(2, word)
-      pstmt.setInt(3, word_count)
-      pstmt.execute()
-    }
-    pstmt.close()
-
-  }
-
-    /** TODO read files in a loop */
-
-
   val mySeq1 = openSource(srcName1)
   val mySeq2 = openSource(srcName2)
   val mySeq3 = openSource(srcName3)
 
-  val beerArray = WordCount(mySeq1) ++ WordCount(mySeq2) ++ WordCount(mySeq3)
+  val beerArray1 = WordCount.getWordCount(mySeq1)
+  val beerArray2 = WordCount.getWordCount(mySeq2)
+  val beerArray3 = WordCount.getWordCount(mySeq3)
+
+  val beerArray = beerArray1 ++ beerArray2 ++ beerArray3
+
   println(beerArray)
-  val writing = writeToDatabase(beerArray,"beersDB.db")
 
-  def mergeSameWord(fName: String) = {
-    val environmentVars = System.getenv()
+  val beerDB = NewDatabase.createNewDatabase()
 
-    //val properties = System.getProperties()
-    val sqlite_home = environmentVars.get("SQLITE_HOME").replace("\\", "/")
+  val writing = writeToDatabase.writeToDatabase(beerArray,"beersDB.db")
 
-    val dbname = fName
-    val url = s"jdbc:sqlite:$sqlite_home/db/$dbname"
-
-    val conn = DriverManager.getConnection(url)
-
-    //lets filter
-    val sql =
-      """
-        |SELECT DISTINCT bt.word, SUM(bt.word_count) WordSum FROM beersTable bt
-        |GROUP BY bt.word
-        |ORDER BY bt.word_count DESC
-        |LIMIT 100""".stripMargin
-
-    val statement = conn.createStatement()
-    val resultSet = statement.executeQuery(sql)
-
-    val meta = resultSet.getMetaData
-    var colSeq = ListBuffer[String]()
-    for (i <- 1 to meta.getColumnCount) {
-      val meta1 = meta.getColumnName(i) + " "
-      print(meta1)
-      colSeq += meta.getColumnName(i)
-    }
-
-    while ( resultSet.next() ) {
-      println()
-      colSeq.map(col => print(resultSet.getString(col) + " "))
-    }
-
-  }
-
-  val mergedResult = mergeSameWord("beersDB.db")
-
+  val mergedResult = mergeSameWords.mergeSameWords("beersDB.db")
 }
